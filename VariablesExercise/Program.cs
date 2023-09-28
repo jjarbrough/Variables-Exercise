f55c11e6977b1fa06b46977339bdd6313d7ba829
James Yarbrough <jjarbrough@gmail.com>
2023-09-28 10:09:32 -0500

    made requested variables and print out line

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String stringVariable = "declared and initialized";
+            int intVAriable = 5;
+            char charVariable = 'Y';
+            bool boolVAriable = true;
+            double doubleVariable = 13.13;
+            string catName = "Sam";
+            int catAge = 4;
+            Console.WriteLine($"My Cats name is {catName}, He is {catAge} years old");
         }
     }
 }
